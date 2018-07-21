3bd5318a33a3fbcd850b34c0dc06305e8f35fcfc
Alexander Babel <us09alex@icloud.com>
2018-07-21 14:50:54 +0200

    Added support for multiple devices

--- index.js
+++ index.js
@@ -1,67 +1,71 @@
-var Service, Characteristic;
-var request = require('request');
+let Service, Characteristic;
+let request = require('request');
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  
-  homebridge.registerAccessory("homebridge-sonoff-tasmota-http", "SonoffTasmotaHTTP", SonoffTasmotaHTTPAccessory);
+
+  homebridge.registerAccessory('homebridge-sonoff-tasmota-http', 'SonoffTasmotaHTTP', SonoffTasmotaHTTPAccessory);
 }
 
 function SonoffTasmotaHTTPAccessory(log, config) {
-  this.log = log;
-  this.config = config;
-  this.name = config["name"]
-  this.relay = config["relay"] || ""
-  this.hostname = config["hostname"] || "sonoff"
-  this.password = config["password"] || "";
-  
-  this.service = new Service.Outlet(this.name);
-  
-  this.service
-  .getCharacteristic(Characteristic.On)
-  .on('get', this.getState.bind(this))
-  .on('set', this.setState.bind(this));
-  
-  this.log("Sonoff Tasmota HTTP Initialized")
+  const self = this;
+  self.log = log;
+
+  config['devices'] = config['devices'] || [];
+  for (let i = 0; i < config['devices'].length; i++) {
+    self.configureAccessory(config['devices'][i]);
+  }
 }
 
-SonoffTasmotaHTTPAccessory.prototype.getState = function(callback) {
-  var that = this
-  request("http://" + that.hostname + "/cm?user=admin&password=" + that.password + "&cmnd=Power" + that.relay, function(error, response, body) {
-    if (error) return callback(error);
-    var sonoff_reply = JSON.parse(body); // {"POWER":"ON"}
-    that.log("Sonoff HTTP: " + that.hostname + ", Relay " + that.relay + ", Get State: " + JSON.stringify(sonoff_reply));
-    switch (sonoff_reply["POWER" + that.relay]) {
-      case "ON":
-        callback(null, 1);
-        break;
-      case "OFF":
-        callback(null, 0);
-        break;
-    }
-  })
+SonoffTasmotaHTTPAccessory.prototype.configureAccessory = function (config) {
+  const self = this;
+  const service = new Service.Outlet(config['name'] || 'Sonoff');
+  service.getCharacteristic(Characteristic.On)
+    .on('get', async (callback) => {
+      const response = await self.sendRequest(service.url);
+      if (!response) {
+        callback(new Error('Could not get state'));
+        return;
+      }
+
+      platform.log(`Get state: ${service.hostname}${service.relay ? `, Relay ${service.relay}` : ''}, Body: ${body}`);
+      callback(null, response['POWER' + service.relay] === 'ON' ? 1 : 0);
+    })
+    .on('set', async (toggle, callback) => {
+      const response = await self.sendRequest(`${service.url}%20${toggle ? 'On' : 'Off'}`);
+      if (!response) {
+        callback(new Error('Could not set state'));
+        return;
+      }
+
+      platform.log(`Set state: ${service.hostname}${service.relay ? `, Relay ${service.relay}` : ''}, Body: ${body}`);
+      callback();
+    });
+
+  service.name = config['name'] || 'Sonoff';
+  service.relay = config['relay'] || '';
+  service.hostname = config['hostname'] || 'sonoff';
+  service.password = config['password'] || '';
+  service.url = `http://${service.hostname}/cm?user=admin&password=${service.password}&cmnd=Power${service.relay}`;
+
+  self.log('Sonoff Tasmota HTTP Initialized');
+  self.services.push(service);
 }
 
-SonoffTasmotaHTTPAccessory.prototype.setState = function(toggle, callback) {
-  var newstate = "%20Off"
-  if (toggle) newstate = "%20On"
-  var that = this
-  request("http://" + that.hostname + "/cm?user=admin&password=" + that.password + "&cmnd=Power" + that.relay + newstate, function(error, response, body) {
-    if (error) return callback(error);
-    var sonoff_reply = JSON.parse(body); // {"POWER":"ON"}
-    that.log("Sonoff HTTP: " + that.hostname + ", Relay " + that.relay + ", Set State: " + JSON.stringify(sonoff_reply));
-    switch (sonoff_reply["POWER" + that.relay]) {
-      case "ON":
-        callback();
-        break;
-      case "OFF":
-        callback();
-        break;
-    }
-  })
+SonoffTasmotaHTTPAccessory.prototype.sendRequest = (url) => {
+  return new Promise(resolve => {
+    request(url, function (error, { }, body) {
+      if (error) {
+        resolve(false);
+        return;
+      }
+
+      resolve(JSON.parse(body));
+    });
+  });
 }
 
-SonoffTasmotaHTTPAccessory.prototype.getServices = function() {
-  return [this.service];
+SonoffTasmotaHTTPAccessory.prototype.getServices = function () {
+  return this.services;
 }
